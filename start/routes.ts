fb10b2a4bae8f9c38cd12ea29d308dddb3d0ad16
BryanCa871 <bryansoe871@gmail.com>
2023-04-14 15:00:43 -0600

    Update routes.ts

--- start/routes.ts
+++ start/routes.ts
@@ -65,5 +65,5 @@ Route.get('api/v2/salones', 'SalonesController.obtenerSalones').middleware(['aut
 Route.post('api/v2/salones/add', 'SalonesController.addSalon').middleware(['auth']);//insertarSalones
 Route.delete('api/v2/salones/:id', 'SalonesController.deleteSalon');//eliminarSalones
 Route.get('/api/v2/salones/UP/:id', 'SalonesController.obtenerSalon')//obtenerSalon
-Route.put('/api/v2/salones/:id', 'SalonController.actualizarSalon')//actualizarSensor
+Route.put('/api/v2/salones/:id', 'SalonesController.actualizarSalon')//actualizarSensor
 
